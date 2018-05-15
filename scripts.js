8ec41bc8efb4e8f8b44a8cb3499f7f2d853f88a5
Prince <k.prince1997@gmail.com>
2018-05-14 17:25:48 -0700

    Added a new navbar

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("navbar-button").style.display = "none";
+
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("navbar-button").style.display = "inline-block";
+}
