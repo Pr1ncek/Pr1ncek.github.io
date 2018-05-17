e065926a674ff7701d8406b4fbfb3f453842e370
Prince <k.prince1997@gmail.com>
2018-05-17 15:37:03 -0700

    Can view projects now. Added more media queries. Courses section has been added.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,44 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-    document.getElementById("navbar-button").style.display = "none";
-
+  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("navbar-button").style.display = "none";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    document.getElementById("navbar-button").style.display = "inline-block";
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("navbar-button").style.display = "inline-block";
 }
+
+$("document").ready(function() {
+  $("#btn1").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#about-section").offset().top
+      },
+      1000
+    );
+  });
+  $("#btn2").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#tech-heading").offset().top
+      },
+      1000
+    );
+  });
+  $("#btn3").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#projects-section").offset().top
+      },
+      1000
+    );
+  });
+  $("#btn4").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $("#contact-section").offset().top
+      },
+      1000
+    );
+  });
+});
